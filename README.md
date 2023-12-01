971fd3e3964ae4a40cdec962f6d296b16e92bee4
Biasiolo <biasiolorafael@gmail.com>
2023-12-01 17:13:07 -0300

    new version

--- README.md
+++ README.md
@@ -1,29 +1,33 @@
 # SCREEN COLOR PLUGIN
 ![Downloads](https://img.shields.io/npm/dm/screen_color_plugin)
+
 Enhance your website's visual appeal with the Screen Color Plugin – a powerful jQuery tool that allows developers to effortlessly integrate a screen color change button into their web pages.
 
 **Check out the live application example at [Screen Color Button](https://screen-color-button.vercel.app/).**
 
-If you found this plugin useful, please consider making a $2 contribution via PayPal to bestkaraokeapp@gmail.com for continuous improvement. Your support is invaluable, and we genuinely appreciate it!
+If you find this plugin valuable, consider contributing to support its continuous improvement. Your donation helps us maintain and enhance the plugin for the community.
 
-[Contribute Now](https://www.paypal.com/donate/?hosted_button_id=9LNPLZ6BQ74YS)
+[![Donate](https://img.shields.io/badge/Donate-$2-brightgreen)](https://www.paypal.com/donate/?hosted_button_id=9LNPLZ6BQ74YS)
 
-### 🚀 New Feature!!
+[![Donation Counter](https://img.shields.io/badge/Donations-35-brightgreen)](https://www.paypal.com/donate/?hosted_button_id=9LNPLZ6BQ74YS)
+
+### 🚀 New Feature - GRADIENT COLORS!!
 Now, experience enhanced customization as the plugin not only changes the background color but also dynamically adjusts the text color for improved contrast. Elevate the visual aesthetics of your websites effortlessly!
 
 ## Description
 The Screen Color Plugin offers an easy-to-use solution for developers seeking to add a screen color change button to their web pages. With this plugin, users can seamlessly customize the background colors and incorporate engaging visual effects for an interactive user experience.
 
 ## Features
-Dynamic Color Change: Choose from a variety of pre-defined colors to personalize your page's appearance.
-Button Sink Effect: Add a subtle and stylish button animation to captivate your audience.
+- **Dynamic Color Change:** Choose from a variety of pre-defined colors to personalize your page's appearance.
+- **Button Sink Effect:** Add a subtle and stylish button animation to captivate your audience.
 
-## How to Use
+## Installation
 
-1. **Installation:**
-   - npm i screen_color_plugin
+1. **NPM:**
+   - Run `npm install screen_color_plugin` to install the plugin.
 
 2. **Initialization:**
+   - Include jQuery and the plugin script in your HTML.
    - Select the desired button using jQuery and call the `scrollColorChange()` method to activate the plugin.
 
    ```html
@@ -42,15 +46,16 @@ Button Sink Effect: Add a subtle and stylish button animation to captivate your 
    </script>
    
 ## Customization
-This plugin is highly customizable—tweak the colors to your liking, play with button effects, and more...
+This plugin is highly customizable—tweak the colors to your liking, play with button effects, and more to create a unique user experience.
+
+## User Experience
+Immerse your users in an engaging journey with the Screen Color Plugin. Create an interactive and visually appealing website that leaves a lasting impression.
 
 ## Contribution
-Feel free to contribute with improvements, bug fixes, or new features. Open an issue or submit a pull request!
-Send a message and let me know how you're using it!
-Let's collaborate!
+Feel free to contribute with improvements, bug fixes, or new features. Open an issue or submit a pull request! Send a message and let us know how you're using it! Let's collaborate!
 
 ## Support
-Our dedicated team is here to assist you, ensuring a seamless and positive experience with our products and services.
+Send me an email.
 
 ## License
-This project is licensed under the MIT License. See the LICENSE file for more details.
+This project is licensed under the MIT License, by Biasiolo.
--- plugin-screen-color.js
+++ plugin-screen-color.js
@@ -1,4 +1,14 @@
 (function ($) {
+    // Função para determinar o contraste de cor
+    function getContrastYIQ(hexcolor) {
+        var r = parseInt(hexcolor.substr(1, 2), 16);
+        var g = parseInt(hexcolor.substr(3, 2), 16);
+        var b = parseInt(hexcolor.substr(5, 2), 16);
+        var yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
+        return (yiq >= 128) ? 'black' : 'white';
+    }
+
+    // Plugin principal
     $.fn.scrollColorChange = function (options) {
         var settings = $.extend({
             colors: [
@@ -9,54 +19,73 @@
                 { name: "red", color: "rgb(214, 40, 40)" },
                 { name: "black", color: "#080808" },
             ],
-            initialColor: "blue", // Adiciona uma nova opção para a cor inicial
-
-            textColors: [ // Add an array of contrasting text colors
-                { name: "blueText", color: "#66250B" }, // Contrast for blue
-                { name: "greenText", color: "#A60D2C" }, // Contrast for green
-                { name: "yellowText", color: "#080808" }, // Contrast for yellow
-                { name: "purpleText", color: "#020827" }, // Contrast for purple
-                { name: "redText", color: "#D9FEBE" }, // Contrast for red
-                { name: "blackText", color: "#fff" }, // Contrast for black
+            gradients: [
+                { name: "blueToGreen", gradient: "linear-gradient(to right, rgb(43, 174, 226), rgb(43, 226, 113))" },
+                { name: "redToPurple", gradient: "linear-gradient(to right, rgb(214, 40, 40), rgb(159, 43, 226))" },
+            ],
+            initialColor: "blue",
+            textColors: [
+                { name: "blueText", color: "#66250B" },
+                { name: "greenText", color: "#A60D2C" },
+                { name: "yellowText", color: "#080808" },
+                { name: "purpleText", color: "#020827" },
+                { name: "redText", color: "#D9FEBE" },
+                { name: "blackText", color: "#fff" },
             ]
         }, options);
 
         // Encontrar o índice da cor inicial
         var initialColorIndex = settings.colors.findIndex(color => color.name === settings.initialColor);
 
-        // Inicia com a cor inicial ou, se não for encontrada, com a primeira cor
+        // Iniciar com a cor inicial ou, se não for encontrada, com a primeira cor
         var currentColorIndex = (initialColorIndex !== -1) ? initialColorIndex : 0;
 
+        // Função para mudar a cor
         function changeColor() {
             currentColorIndex = (currentColorIndex + 1) % settings.colors.length;
             updateColor();
         }
 
-
-
+        // Função para atualizar a cor e aplicar a cor de texto contrastante
         function updateColor() {
-            var selectedColor = settings.colors[currentColorIndex].color;
-            var selectedTextColor = settings.textColors[currentColorIndex].color;
+            var selectedColor, selectedTextColor;
+
+            if (settings.colors[currentColorIndex].gradient) {
+                // Se estiver usando gradiente, aplique-o diretamente
+                selectedColor = settings.colors[currentColorIndex].gradient;
+                // Defina a cor do texto como branca por padrão
+                selectedTextColor = "#fff";
+            } else {
+                // Se não estiver usando gradiente, use a cor sólida
+                selectedColor = settings.colors[currentColorIndex].color;
+                selectedTextColor = settings.textColors[currentColorIndex].color;
+            }
+
             $("body").css({
-                "background-color": selectedColor,
-                "color": selectedTextColor // Set the contrasting text color
+                "background": selectedColor,
+                "color": selectedTextColor
             });
 
-            var textColor = getContrastYIQ(selectedColor);
-            $("body").css("color", textColor); // Set text color to ensure contrast
+            if (!settings.colors[currentColorIndex].gradient) {
+                // Apenas se não estiver usando gradiente, determine a cor do texto contrastante
+                var textColor = getContrastYIQ(selectedColor);
+                $("body").css("color", textColor);
+            }
 
-            // Adiciona a classe 'rotacionar' ao display
-            $(".scroll-container").addClass("rotacionar");
+            // Adicionar a classe 'rotate' ao contêiner
+            $(".scroll-container").addClass("rotate");
 
-            // Remove a classe 'rotacionar' após 1 segundo
-            setTimeout(function () {
-                $(".scroll-container").removeClass("rotacionar");
-            }, 1000);
+            // Remover a classe 'rotate' após a transição
+            $(".scroll-container").one("transitionend", function () {
+                $(this).removeClass("rotate");
+            });
         }
 
+        // Iterar sobre cada elemento correspondente
         return this.each(function () {
             var $this = $(this);
 
+            // Aplicar estilos iniciais ao elemento
             $this.css({
                 fontSize: "12px",
                 margin: "3px",
@@ -65,22 +94,23 @@
                 color: "white",
                 border: "none",
                 borderRadius: "5px",
-                transition: "background-color 0.3s, transform 0.3s",
+                transition: "background 0.3s, transform 0.3s",
             });
 
+            // Lidar com o evento de clique para mudar a cor
             $this.on("click", function () {
                 changeColor();
             });
 
+            // Lidar com a tecla de seta para baixo para mudar a cor
             $(document).keydown(function (e) {
                 if (e.which === 40) {
                     changeColor();
                 }
             });
 
-            // Inicia com a cor inicial
+            // Iniciar com a cor inicial
             updateColor();
         });
     };
 })(jQuery);
-
